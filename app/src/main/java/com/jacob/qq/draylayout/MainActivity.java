3f014f7d1f42dbcd695d993bfd1ac7689f4d5818
jacob-wj <wangjia199055@126.com>
2015-04-16 09:46:41 +0800

    init project

---
+++ app/src/main/java/com/jacob/qq/draylayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jacob.qq.draylayout;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
